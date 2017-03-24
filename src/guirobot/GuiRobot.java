868ad0c235d73c4978842915feb76ae294e92f14
Unknown <mikel.aretxabaleta@gmail.com>
2017-03-24 23:15:29 +0100

    2 jugadores terminado sin panel de partidas ganadas

--- src/guirobot/GuiRobot.java
+++ src/guirobot/GuiRobot.java
@@ -5,6 +5,13 @@
  */
 package guirobot;
 
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.*;
+
 /**
  *
  * @author 7FPROG07
@@ -22,10 +29,35 @@ public class GuiRobot {
 
     public static void start() {
         pm.setVisible(true);
-	pm.reshape(400, 400, 750, 875);
-        pm.getjLabel2().reshape(0, 0, 75, 75);
 	
-        //pi.setVisible();
+	//pm.getContentPane().setBackground(Color.blue);
+        
+	pm.setContentPane(pm.getjLabel7());
+	pm.getContentPane().add(pm.getjLabel2()).setBounds(0, 0, 75, 75);
+	pm.getContentPane().add(pm.getjLabel1()).setBounds(675, 675, 75, 75);
+	pm.getContentPane().add(pm.getjLabel1()).setLocation(675, 675);
+	pm.getContentPane().add(pm.getjLabel3()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel4()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel5()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel6()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel8()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel9()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel10()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().add(pm.getjLabel11()).setBounds(0, 0, 0, 0);
+	pm.getContentPane().setBounds(0, 0, 750, 750);
+	pm.setBounds(0, 0, 750, 875);
+	/*
+	BufferedImage img = null;
+try {
+    img = ImageIO.read(new File("sky.png"));
+} catch (IOException e) {
+}
+	pm.setIconImage(img);
+	  
+		
+*/
+
+//pi.setVisible();
 	
        
     }
--- src/guirobot/PanelMovimientos.java
+++ src/guirobot/PanelMovimientos.java
@@ -6,13 +6,16 @@
 package guirobot;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -21,12 +24,18 @@ import javax.swing.Timer;
  * @author 7FPROG07
  */
 public class PanelMovimientos extends javax.swing.JFrame {
-    
 
+	
+    
+    
     int jLabel3set=0;
     int jLabel4set=0;
     int jLabel5set=0;
     int jLabel6set=0;
+    int jLabel8set=0;
+    int jLabel9set=0;
+    int jLabel10set=0;
+    int jLabel11set=0;
     
     
     
@@ -34,6 +43,7 @@ public class PanelMovimientos extends javax.swing.JFrame {
      * Creates new form PanelMovimientos
      */
     public PanelMovimientos() {
+		
         
 	  
        
@@ -53,14 +63,21 @@ public class PanelMovimientos extends javax.swing.JFrame {
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
+                jLabel7 = new javax.swing.JLabel();
                 jLabel2 = new javax.swing.JLabel();
                 jLabel1 = new javax.swing.JLabel();
                 jLabel3 = new javax.swing.JLabel();
                 jLabel4 = new javax.swing.JLabel();
                 jLabel5 = new javax.swing.JLabel();
                 jLabel6 = new javax.swing.JLabel();
+                jOptionPane1 = new javax.swing.JOptionPane();
+                jLabel8 = new javax.swing.JLabel();
+                jLabel9 = new javax.swing.JLabel();
+                jLabel10 = new javax.swing.JLabel();
+                jLabel11 = new javax.swing.JLabel();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+                setBackground(new java.awt.Color(52, 45, 201));
                 setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
                 setMaximumSize(new java.awt.Dimension(750, 750));
                 setMinimumSize(new java.awt.Dimension(750, 750));
@@ -74,69 +91,68 @@ public class PanelMovimientos extends javax.swing.JFrame {
                                 formKeyPressed(evt);
                         }
                 });
+                getContentPane().setLayout(new javax.swing.OverlayLayout(getContentPane()));
+
+                jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/sky.png"))); // NOI18N
+                getContentPane().add(jLabel7);
 
                 jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/robot.png"))); // NOI18N
                 jLabel2.setFocusCycleRoot(true);
                 jLabel2.setFocusTraversalPolicyProvider(true);
                 jLabel2.setNextFocusableComponent(jLabel2);
+                jLabel2.setOpaque(true);
+                getContentPane().add(jLabel2);
 
+                jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
                 jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/robot.png"))); // NOI18N
+                jLabel1.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+                jLabel1.setOpaque(true);
+                getContentPane().add(jLabel1);
 
                 jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.TRAILING);
                 jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
                 jLabel3.setMinimumSize(new java.awt.Dimension(0, 0));
                 jLabel3.setPreferredSize(new java.awt.Dimension(0, 0));
+                getContentPane().add(jLabel3);
 
                 jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
                 jLabel4.setPreferredSize(new java.awt.Dimension(0, 0));
+                getContentPane().add(jLabel4);
 
                 jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
                 jLabel5.setPreferredSize(new java.awt.Dimension(0, 0));
+                getContentPane().add(jLabel5);
 
                 jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
                 jLabel6.setPreferredSize(new java.awt.Dimension(0, 0));
+                getContentPane().add(jLabel6);
 
-                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-                getContentPane().setLayout(layout);
-                layout.setHorizontalGroup(
-                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(layout.createSequentialGroup()
-                                .addGap(109, 109, 109)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(641, Short.MAX_VALUE))
-                );
-                layout.setVerticalGroup(
-                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jLabel2)
-                                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(82, 82, 82)
-                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(62, 62, 62)
-                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(80, 80, 80)
-                                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 376, Short.MAX_VALUE)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                );
+                jOptionPane1.setMessage("You win!!!!");
+                getContentPane().add(jOptionPane1);
+
+                jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
+                jLabel8.setText("jLabel8");
+                getContentPane().add(jLabel8);
+
+                jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
+                jLabel9.setText("jLabel9");
+                getContentPane().add(jLabel9);
+
+                jLabel10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
+                jLabel10.setText("jLabel10");
+                getContentPane().add(jLabel10);
+
+                jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/guirobot/fire.png"))); // NOI18N
+                jLabel11.setText("jLabel11");
+                getContentPane().add(jLabel11);
 
                 pack();
         }// </editor-fold>//GEN-END:initComponents
 
     private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
         int c = evt.getKeyCode();
+	
+	//JUGADOR 1
 	
         if( c == java.awt.event.KeyEvent.VK_P){
           if (GuiRobot.pm.getjLabel2().getLocation().x<675 && GuiRobot.pm.getjLabel2().getLocation().x>=0)
@@ -184,6 +200,55 @@ public class PanelMovimientos extends javax.swing.JFrame {
               jLabel6set=0;}
 	
         }
+	
+	//JUGADOR 2
+	
+	if( c == java.awt.event.KeyEvent.VK_RIGHT){
+          if (GuiRobot.pm.getjLabel1().getLocation().x<675 && GuiRobot.pm.getjLabel1().getLocation().x>=0)
+        GuiRobot.pm.getjLabel1().setLocation((GuiRobot.pm.getjLabel1().getLocation().x+75), GuiRobot.pm.getjLabel1().getLocation().y);  
+        }
+        
+        
+        if( c == java.awt.event.KeyEvent.VK_LEFT){
+        if (GuiRobot.pm.getjLabel1().getLocation().x<=750 && GuiRobot.pm.getjLabel1().getLocation().x>74)
+        GuiRobot.pm.getjLabel1().setLocation((GuiRobot.pm.getjLabel1().getLocation().x-75), GuiRobot.pm.getjLabel1().getLocation().y);
+        }
+        if( c == java.awt.event.KeyEvent.VK_DOWN){
+        if (GuiRobot.pm.getjLabel1().getLocation().y<675 && GuiRobot.pm.getjLabel1().getLocation().y>=0)
+        GuiRobot.pm.getjLabel1().setLocation((GuiRobot.pm.getjLabel1().getLocation().x), GuiRobot.pm.getjLabel1().getLocation().y+75);    
+        }
+        if( c == java.awt.event.KeyEvent.VK_UP){
+        if (GuiRobot.pm.getjLabel1().getLocation().y<=750 && GuiRobot.pm.getjLabel1().getLocation().y>74)
+        GuiRobot.pm.getjLabel1().setLocation((GuiRobot.pm.getjLabel1().getLocation().x), GuiRobot.pm.getjLabel1().getLocation().y-75);
+        }
+        if( c == java.awt.event.KeyEvent.VK_ENTER){
+        if (jLabel8set==0){
+		
+        GuiRobot.pm.jLabel8.reshape((GuiRobot.pm.getjLabel1().getLocation().x-75), GuiRobot.pm.getjLabel1().getLocation().y, 75, 75);
+	jLabel8set=1;
+	timer5.start();
+	}
+	else if (jLabel9set==0){
+        GuiRobot.pm.jLabel9.reshape((GuiRobot.pm.getjLabel1().getLocation().x-75), GuiRobot.pm.getjLabel1().getLocation().y, 75, 75);
+	jLabel9set=1;
+	timer6.start();
+	}
+	else if (jLabel10set==0){
+        GuiRobot.pm.jLabel10.reshape((GuiRobot.pm.getjLabel1().getLocation().x-75), GuiRobot.pm.getjLabel1().getLocation().y, 75, 75);
+	jLabel10set=1;
+	timer7.start();
+	}
+	else if (jLabel11set==0){
+        GuiRobot.pm.jLabel11.reshape((GuiRobot.pm.getjLabel1().getLocation().x-75), GuiRobot.pm.getjLabel1().getLocation().y, 75, 75);
+	jLabel11set=1;
+	timer8.start();
+	}
+	else {jLabel8set=0;
+              jLabel9set=0;
+              jLabel10set=0;
+              jLabel11set=0;}
+	
+        }
     }//GEN-LAST:event_formKeyPressed
 
     private void formKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyTyped
@@ -227,11 +292,17 @@ public class PanelMovimientos extends javax.swing.JFrame {
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JLabel jLabel1;
+        private javax.swing.JLabel jLabel10;
+        private javax.swing.JLabel jLabel11;
         private javax.swing.JLabel jLabel2;
         private javax.swing.JLabel jLabel3;
         private javax.swing.JLabel jLabel4;
         private javax.swing.JLabel jLabel5;
         private javax.swing.JLabel jLabel6;
+        private javax.swing.JLabel jLabel7;
+        private javax.swing.JLabel jLabel8;
+        private javax.swing.JLabel jLabel9;
+        private javax.swing.JOptionPane jOptionPane1;
         // End of variables declaration//GEN-END:variables
 
 	public JLabel getjLabel1() {
@@ -253,6 +324,7 @@ public class PanelMovimientos extends javax.swing.JFrame {
     public JLabel getjLabel3() {
         return jLabel3;
     }
+    
 
     public void setjLabel3(JLabel jLabel3) {
         this.jLabel3 = jLabel3;
@@ -281,7 +353,60 @@ public class PanelMovimientos extends javax.swing.JFrame {
 	public void setjLabel6(JLabel jLabel6) {
 		this.jLabel6 = jLabel6;
 	}
+
+	public JLabel getjLabel7() {
+		return jLabel7;
+	}
+
+	public void setjLabel7(JLabel jLabel7) {
+		this.jLabel7 = jLabel7;
+	}
+
+	public JOptionPane getjOptionPane1() {
+		return jOptionPane1;
+	}
+
+	public void setjOptionPane1(JOptionPane jOptionPane1) {
+		this.jOptionPane1 = jOptionPane1;
+	}
+
+	public JLabel getjLabel10() {
+		return jLabel10;
+	}
+
+	public void setjLabel10(JLabel jLabel10) {
+		this.jLabel10 = jLabel10;
+	}
+
+	public JLabel getjLabel11() {
+		return jLabel11;
+	}
+
+	public void setjLabel11(JLabel jLabel11) {
+		this.jLabel11 = jLabel11;
+	}
+
+	public JLabel getjLabel8() {
+		return jLabel8;
+	}
+
+	public void setjLabel8(JLabel jLabel8) {
+		this.jLabel8 = jLabel8;
+	}
+
+	public JLabel getjLabel9() {
+		return jLabel9;
+	}
+
+	public void setjLabel9(JLabel jLabel9) {
+		this.jLabel9 = jLabel9;
+	}
+
+	
+
+	
     
+
     
         
 		
@@ -289,7 +414,9 @@ public class PanelMovimientos extends javax.swing.JFrame {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		GuiRobot.pm.jLabel3.reshape((GuiRobot.pm.getjLabel3().getLocation().x+75), GuiRobot.pm.getjLabel3().getLocation().y, 75, 75);
-		//To change body of generated methods, choose Tools | Templates.
+		if(GuiRobot.pm.getjLabel3().getLocation().x==GuiRobot.pm.getjLabel1().getLocation().x &&  GuiRobot.pm.getjLabel3().getLocation().y==GuiRobot.pm.getjLabel1().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER1 WINS!!!!");
+//To change body of generated methods, choose Tools | Templates.
 	}};
 	Timer timer1 = new Timer(200, al1);
     
@@ -297,7 +424,9 @@ public class PanelMovimientos extends javax.swing.JFrame {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		GuiRobot.pm.jLabel4.reshape((GuiRobot.pm.getjLabel4().getLocation().x+75), GuiRobot.pm.getjLabel4().getLocation().y, 75, 75);
-		//To change body of generated methods, choose Tools | Templates.
+                if(GuiRobot.pm.getjLabel4().getLocation().x==GuiRobot.pm.getjLabel1().getLocation().x &&  GuiRobot.pm.getjLabel4().getLocation().y==GuiRobot.pm.getjLabel1().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER1 WINS!!!!");		
+//To change body of generated methods, choose Tools | Templates.
 	}};
 	Timer timer2 = new Timer(200, al2);
     
@@ -305,7 +434,9 @@ public class PanelMovimientos extends javax.swing.JFrame {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		GuiRobot.pm.jLabel5.reshape((GuiRobot.pm.getjLabel5().getLocation().x+75), GuiRobot.pm.getjLabel5().getLocation().y, 75, 75);
-		//To change body of generated methods, choose Tools | Templates.
+                if(GuiRobot.pm.getjLabel5().getLocation().x==GuiRobot.pm.getjLabel1().getLocation().x &&  GuiRobot.pm.getjLabel5().getLocation().y==GuiRobot.pm.getjLabel1().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER1 WINS!!!!");		
+//To change body of generated methods, choose Tools | Templates.
 	}};
 	Timer timer3 = new Timer(200, al3);
 
@@ -313,11 +444,53 @@ public class PanelMovimientos extends javax.swing.JFrame {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		GuiRobot.pm.jLabel6.reshape((GuiRobot.pm.getjLabel6().getLocation().x+75), GuiRobot.pm.getjLabel6().getLocation().y, 75, 75);
-		//To change body of generated methods, choose Tools | Templates.
+                if(GuiRobot.pm.getjLabel6().getLocation().x==GuiRobot.pm.getjLabel1().getLocation().x &&  GuiRobot.pm.getjLabel6().getLocation().y==GuiRobot.pm.getjLabel1().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER1 WINS!!!!");		
+//To change body of generated methods, choose Tools | Templates.
 	}};
 	Timer timer4 = new Timer(200, al4);
     
-        
+        //PLAYER 2
+	
+	ActionListener al5=new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		GuiRobot.pm.jLabel8.reshape((GuiRobot.pm.getjLabel8().getLocation().x-75), GuiRobot.pm.getjLabel8().getLocation().y, 75, 75);
+		if(GuiRobot.pm.getjLabel8().getLocation().x==GuiRobot.pm.getjLabel2().getLocation().x &&  GuiRobot.pm.getjLabel8().getLocation().y==GuiRobot.pm.getjLabel2().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER2 WINS!!!!");
+//To change body of generated methods, choose Tools | Templates.
+	}};
+	Timer timer5 = new Timer(200, al5);
+    
+	ActionListener al6=new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		GuiRobot.pm.jLabel9.reshape((GuiRobot.pm.getjLabel9().getLocation().x-75), GuiRobot.pm.getjLabel9().getLocation().y, 75, 75);
+                if(GuiRobot.pm.getjLabel9().getLocation().x==GuiRobot.pm.getjLabel2().getLocation().x &&  GuiRobot.pm.getjLabel9().getLocation().y==GuiRobot.pm.getjLabel2().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER2 WINS!!!!");		
+//To change body of generated methods, choose Tools | Templates.
+	}};
+	Timer timer6 = new Timer(200, al6);
+    
+	ActionListener al7=new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		GuiRobot.pm.jLabel10.reshape((GuiRobot.pm.getjLabel10().getLocation().x-75), GuiRobot.pm.getjLabel10().getLocation().y, 75, 75);
+                if(GuiRobot.pm.getjLabel10().getLocation().x==GuiRobot.pm.getjLabel2().getLocation().x &&  GuiRobot.pm.getjLabel10().getLocation().y==GuiRobot.pm.getjLabel2().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER2 WINS!!!!");		
+//To change body of generated methods, choose Tools | Templates.
+	}};
+	Timer timer7 = new Timer(200, al7);
+
+	ActionListener al8=new ActionListener() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		GuiRobot.pm.jLabel11.reshape((GuiRobot.pm.getjLabel11().getLocation().x-75), GuiRobot.pm.getjLabel11().getLocation().y, 75, 75);
+                if(GuiRobot.pm.getjLabel11().getLocation().x==GuiRobot.pm.getjLabel2().getLocation().x &&  GuiRobot.pm.getjLabel11().getLocation().y==GuiRobot.pm.getjLabel2().getLocation().y)
+			JOptionPane.showMessageDialog(GuiRobot.pm, "PLAYER2 WINS!!!!");		
+//To change body of generated methods, choose Tools | Templates.
+	}};
+	Timer timer8 = new Timer(200, al8);
 
 
 
